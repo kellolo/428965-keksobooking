2f018f5a1e9385441b29aea3b0260bfa7954c9a5
jaredKotleta <87kell@gmail.com>
2018-02-17 08:51:45 +1200

    2

--- js/map.js
+++ js/map.js
@@ -1,14 +1,11 @@
 'use strict';
 var USERS = 8;
-var TITLES = ["Большая уютная квартира", 
-"Маленькая неуютная квартира", "Огромный прекрасный дворец", 
-"Маленький ужасный дворец", "Красивый гостевой домик", "Некрасивый негостеприимный домик", 
-"Уютное бунгало далеко от моря", "Неуютное бунгало по колено в воде"];
+var TITLES = ['Большая уютная квартира', 'Маленькая неуютная квартира', 'Огромный прекрасный дворец', 'Маленький ужасный дворец', 'Красивый гостевой домик', 'Некрасивый негостеприимный домик', 'Уютное бунгало далеко от моря', 'Неуютное бунгало по колено в воде'];
 var TYPE = ['flat', 'house', 'bungalo'];
 var CHECKIN = ['12:00', '13:00', '14:00'];
 var CHECKOUT = ['12:00', '13:00', '14:00'];
-var FEATURES = ["wifi", "dishwasher", "parking", "washer", "elevator", "conditioner"];
-var PHOTOS = ["http://o0.github.io/assets/images/tokyo/hotel1.jpg", "http://o0.github.io/assets/images/tokyo/hotel2.jpg", "http://o0.github.io/assets/images/tokyo/hotel3.jpg"];
+var FEATURES = ['wifi', 'dishwasher', 'parking', 'washer', 'elevator', 'conditioner'];
+var PHOTOS = ['http://o0.github.io/assets/images/tokyo/hotel1.jpg', 'http://o0.github.io/assets/images/tokyo/hotel2.jpg', 'http://o0.github.io/assets/images/tokyo/hotel3.jpg'];
 var MAXROOMS = 5;
 
 
@@ -21,7 +18,7 @@ var getRandom = function (min, max) {
   return Math.floor(min + Math.random() * max);
 };
 
-var generateData = function (shuffle, createUsers, users, xLocation, yLocation, avatar, title, price, uType, rooms, guests, checkIn, checkOut, photos, totalFeatures, feature) {
+var generateData = function (shuffle, createUsers, users, xLocation, yLocation, avatar, title, price, uType, rooms, guests, checkIn, checkOut, photos, totalFeatures) {
 
   avatar = [];
   title = [];
@@ -34,7 +31,7 @@ var generateData = function (shuffle, createUsers, users, xLocation, yLocation, 
   users = [];
   totalFeatures = [];
 
-  //ФУНКЦИЯ СОЗДАНИЯ МАССИВОВ ДАННЫХ ПО КАТЕГОРИЯМ С ЧАСТИЧНО НЕПОВТОРЯЮЩИМИСЯ МАССИВАМИ
+  // ФУНКЦИЯ СОЗДАНИЯ МАССИВОВ ДАННЫХ ПО КАТЕГОРИЯМ С ЧАСТИЧНО НЕПОВТОРЯЮЩИМИСЯ МАССИВАМИ
 
   shuffle = function (usersData, ava, tit, uTy, roo, gue, chIn, chOut, phot, totFeat, feat) {
     for (var j = 0; j < USERS;) {
@@ -69,7 +66,6 @@ var generateData = function (shuffle, createUsers, users, xLocation, yLocation, 
         totalFeatures.push(totFeat);
 
         j++;
-
       } 
     }
 
@@ -85,59 +81,58 @@ var generateData = function (shuffle, createUsers, users, xLocation, yLocation, 
       features: totalFeatures,
       photos: photos
     };
-    
-    return usersData; //возвращает объект массивов данных по категориям
+
+    return usersData; // возвращает объект массивов данных по категориям
   };
 
 
-  //ФУНКЦИЯ СОРТИРОВКИ СГЕНЕРИРОВАННЫХ ДАННЫХ ПО КОНКРЕТНЫМ ПОЛЬЗОВАТЕЛЯМ
+  // ФУНКЦИЯ СОРТИРОВКИ СГЕНЕРИРОВАННЫХ ДАННЫХ ПО КОНКРЕТНЫМ ПОЛЬЗОВАТЕЛЯМ
 
   createUsers = function (incoming, newUser) {
-      incoming = shuffle();
+    incoming = shuffle();
 
-      while (users.length < USERS) {
-        var xLocation = getRandom(300, 600);
-        var yLocation = getRandom(300, 350);
+    while (users.length < USERS) {
+      var xLocation = getRandom(300, 600);
+      var yLocation = getRandom(300, 350);
 
-        newUser = {
-          author: {
-            avatar: 'img/avatars/user0' + incoming.avatars[users.length] + '.png'
-          },
-          location: {
-            x: xLocation,
-            y: yLocation
-          },
-          offer: {
-            title: incoming.titles[users.length],
-            adress: xLocation  + ', ' + yLocation,
-            price: getRandom(1000, 900000),
-            type: incoming.types[users.length],
-            rooms: incoming.rooms[users.length],
-            guests: incoming.guests[users.length],
-            checkin: incoming.in[users.length],
-            checkout: incoming.out[users.length],
-            features: incoming.features[users.length],
-            description: '',
-            photos: incoming.photos[users.length]
-          }
-        };
+      newUser = {
+        author: {
+          avatar: 'img/avatars/user0' + incoming.avatars[users.length] + '.png'
+        },
+        location: {
+          x: xLocation,
+          y: yLocation
+        },
+        offer: {
+          title: incoming.titles[users.length],
+          adress: xLocation + ', ' + yLocation,
+          price: getRandom(1000, 900000),
+          type: incoming.types[users.length],
+          rooms: incoming.rooms[users.length],
+          guests: incoming.guests[users.length],
+          checkin: incoming.in[users.length],
+          checkout: incoming.out[users.length],
+          features: incoming.features[users.length],
+          description: '',
+          photos: incoming.photos[users.length]
+        }
+      };
 
-        users.push(newUser);
-      }
+      users.push(newUser);
+    }
   };
-  
+
   createUsers();
 
-  return users; //возвращает массив объектов по пользователям
+  return users; // возвращает массив объектов по пользователям
 };
 
-var userTemplate;
 var fragment = document.createDocumentFragment();
 
 var generateTemplate = function (button, avatar, users) {
   users = generateData();
 
-  for (var i = 0; i < USERS; i++){
+  for (var i = 0; i < USERS; i++) {
     button = document.createElement('button');
     avatar = document.createElement('img');
     button.style.left = users[i].location.x + 'px';
@@ -155,7 +150,7 @@ var generateTemplate = function (button, avatar, users) {
 };
 
 
-generateTemplate ();
+generateTemplate();
 
 var mapTarget = document.querySelector('.map__pins');
 mapTarget.appendChild(fragment);
